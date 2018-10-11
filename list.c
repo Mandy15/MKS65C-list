62f54e9152ef1844d05cdd6f0724b801846f531f
Mandy <mmai1@stuy.edu>
2018-10-11 01:49:57 -0400

    update

--- list.c
+++ list.c
@@ -5,22 +5,26 @@
 
 void print_list(struct node *n){
 	while(n){
-		printf("%s\n", n->s);
+		printf("%s ", n->s);
 		n = n->next;
 	}
 	printf("\n");
 }
 
 struct node * insert_front(struct node *n, char str[32]){
-	struct node *x = malloc(32);
+	struct node *x = malloc(sizeof(struct node));
 	strcpy(x->s, str);
 	x->next = n;
 	return x;
 }
 
 struct node * free_list(struct node *n){
-	while(n->next != NULL){
-		n = n->next;
+	if(n){
+		struct node *x = n->next;
+		free(n);
+		free_list(x);
+	}else{
+		return n;
 	}
-	return n;
+	return NULL;
 }
--- test.c
+++ test.c
@@ -2,15 +2,25 @@
 #include "list.h"
 
 int main(){
-	struct node *list = NULL;
+	struct node *head = NULL;
 
-	printf("Print list:\n");
-	print_list(list);
+	printf("Print empty list:\n");
+	print_list(head);
 
-	printf("Add new node to beginning:\n");
-	print_list(insert_front(list, "hey"));
+	printf("\nAdd new node to beginning:\n");
+	head = insert_front(head, "hello");
+	print_list(head);
 
-	// printf("Free list:\n");
-	// print_list(free_list(list));
+	printf("\nAdd a second new node to beginning:\n");
+	head = insert_front(head, "hey");
+	print_list(head);
+
+	printf("\nAdd a third new node to beginning:\n");
+	head = insert_front(head, "hi");
+	print_list(head);
+
+	printf("\nFree list:\n");
+	print_list(free_list(head));
+
 	return 0;
 }
